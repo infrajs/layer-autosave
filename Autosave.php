3de767acb0aa00f2d78b04b38254d91378643ae2
Антон Кияткин <a.kiyatkin@itlf.ru>
2015-12-07 19:58:22 +0400

    ns

---
+++ Autosave.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace infrajs\controller\ext;
+
+//autosave, autosaveclient, autosavename
+
+//Из-за этого нельзя кэшировать снимок всей страницы
+class autosave
+{
+	public function get(&$layer, $name = '', $def = null)
+	{
+		if (@is_null($layer['autosavename'])) {
+			return $def;
+		}
+		$val = infra_session_get($layer['autosavename'].'.'.$name);
+		if (@is_null($val)) {
+			return $def;
+		}
+
+		return $val;
+	}
+}
