5b719072502cde57230aec04988799c01e41d0b1
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-12 20:31:19 +0400

    fix

--- Autosave.js
+++ Autosave.js
@@ -1,4 +1,3 @@
-import { Controller } from '/vendor/infrajs/controller/src/Controller.js'
 import { CDN } from '/vendor/akiyatkin/load/CDN.js'
 import { Session } from '/vendor/infrajs/session/Session.js'
 //Скрипт. Точка это разделитель. Могут проблемы когда имя свойства файл расширением,
@@ -21,32 +20,30 @@ let Autosave = {
 		//layer.autosave={};
 		Session.set(layer.autosavename);
 	},
-	get: function (layer, name, def) { //blinds
-		if (!layer.autosavename) return def;
+	get: function (autosavename, name, def) { //blinds
+		if (!autosavename) return def;
 		if (!name) name = '';
-		var val = Session.get(layer.autosavename + '.' + name, def);
+		var val = Session.get(autosavename + '.' + name, def);
 		return val;
 	},
 	logout: function () {//нет возможности востановить значения по умолчанию указанные в слоях.
 		Session.logout();
 		location.href = location.href;//Чтобы сбросить autosave в слоях
 	},
-	set: function (layer, name, val) {//skoroskidka, rte.layer.js
-		if (!layer.autosavename) return;
-		Session.set(layer.autosavename + '.' + name, val);
-
+	set: function (autosavename, name, val) {//skoroskidka, rte.layer.js
+		Session.set(autosavename + '.' + name, val);
 		//var right=infra.seq.right(name);
 		//layer.autosave=infra.seq.set(layer.autosave,right,val);
 	},
 	//-----------
-	loadAll: async layer => {
+	loadAll: async (autosavename, div) => {
 		await CDN.load('jquery')
-		var inps = Autosave.getInps(layer.div).filter('[autosave]');
+		var inps = Autosave.getInps(div).filter('[autosave]');
 		inps.each(function () {
 			var inp = $(this);
 			var name = inp.attr('name');
 			var val = Autosave.getVal(inp);
-			var valsave = Autosave.get(layer, name);
+			var valsave = Autosave.get(autosavename, name);
 			if (valsave !== undefined) { //Значения по умолчанию подставляемые браузером
 				Autosave.setVal(inp, valsave);
 				Autosave.bracket(inp, true);
@@ -55,10 +52,10 @@ let Autosave = {
 		});
 
 	},
-	saveAll: async function (layer) {
-		if (!layer.autosavename) return;
+	/*saveAll: async function (autosavename, div) {
+		if (!autosavename) return;
 		await CDN.load('jquery')
-		var inps = Autosave.getInps(layer.div).filter('[autosave]');
+		var inps = Autosave.getInps(div).filter('[autosave]');
 
 		inps.each(function () {
 			var inp = $(this);
@@ -66,13 +63,13 @@ let Autosave = {
 			if (!name) return;
 			//this.removeAttribute('notautosaved');//должно быть отдельное событие которое при малейшем измееннии поля ввода будет удалять это свойство //Если свойства этого нет, то сохранять ничего не нужно
 			var val = Autosave.getVal(inp);
-			var nowval = Autosave.get(layer, name);
+			var nowval = Autosave.get(layer.autosavename, name);
 			if (!nowval) nowval = '';
 			if (val == nowval) return;
 			Autosave.bracket(inp, true);
-			Autosave.set(layer, name, val);
+			Autosave.set(layer.autosavename, name, val);
 		});
-	},
+	},*/
 	getVal: function (inp) {
 		inp = $(inp);
 		if (inp.attr('type') == 'checkbox') {
@@ -133,68 +130,48 @@ let Autosave = {
 			evt.initEvent(event, true, true); // event type,bubbling,cancelable
 			return !element.dispatchEvent(evt);
 		}
+	},
+	init: async function (autosavename, div) {
+		await CDN.load('jquery')
+		var inps = Autosave.getInps(div).not('[autosave]').attr('autosave', 1);//Берём input тольо не обработанные
+	
+		inps.each(function () {
+			var inp = this;
+			var html = '<div class="autosavebreak" title="Отменить изменения" style="display:none; position:absolute; width:9px; height:3px; cursor:pointer; background-color:gray;"onmouseout="this.style.backgroundColor=\'gray\'" onmouseover="this.style.backgroundColor=\'red\'"></div>';
+			if (inp.getAttribute('autosavebreak')) {
+				inp.removeAttribute('autosavebreak');
+				$(inp).before(html);
+				var def = Autosave.getVal(inp);
+				$(inp).prevAll('.autosavebreak:first').click(function () {
+					Autosave.setVal(inp, def);
+					//$(inp).change();//Применится для визуального редактора
+					Autosave.fireEvent(inp, 'change');
+					Autosave.set(autosavename, inp.name, undefined);//В сессии установится null 
+					Autosave.bracket(inp, false);//Скрываем пипку сбороса сохранённого
+				});
+			}
+		});
+		//Функция сохраняет все значение, а не только того элемента на ком она сработала
+	
+		Autosave.loadAll(autosavename, div);//Востанавливаем то что есть в autosave, При установки нового занчения срабатывает change
+		//change может программно вызываться у множества элементов. это приводит к тормозам.. нужно объединять
+		inps.change(function () {//Всё на change.. при авто изменении нужно вызывать событие change
+			//Autosave.saveAll(layer);
+			var inp = $(this);
+			var name = inp.attr('name');//getInps проверяет чтобы у всех были name
+			//this.removeAttribute('notautosaved');//должно быть отдельное событие которое при малейшем измееннии поля ввода будет удалять это свойство //Если свойства этого нет, то сохранять ничего не нужно
+	
+			var val = Autosave.getVal(inp);
+			//var nowval=Autosave.get(layer.autosavename,name);
+			//if(!nowval)nowval='';
+			//if(val===nowval)return;
+			Autosave.bracket(inp, true);
+			Autosave.set(autosavename, name, val);
+		});//Подписались на события inputов onchange
 	}
 };
-let autosave = Autosave;
-
-Controller.autosaveHand = async function (layer) {
-	if (layer.autosavenametpl) layer.autosavename = Template.parse([layer.autosavenametpl], layer);
-	if (!layer.autosavename) return;
-	await CDN.load('jquery')
-	var inps = Autosave.getInps(layer.div).not('[autosave]').attr('autosave', 1);//Берём input тольо не обработанные
-
-	inps.each(function () {
-		var inp = this;
-		var html = '<div class="autosavebreak" title="Отменить изменения" style="display:none; position:absolute; width:9px; height:3px; cursor:pointer; background-color:gray;"onmouseout="this.style.backgroundColor=\'gray\'" onmouseover="this.style.backgroundColor=\'red\'"></div>';
-		if (inp.getAttribute('autosavebreak')) {
-			inp.removeAttribute('autosavebreak');
-			$(inp).before(html);
-			var def = Autosave.getVal(inp);
-			$(inp).prevAll('.autosavebreak:first').click(function () {
-				Autosave.setVal(inp, def);
-				//$(inp).change();//Применится для визуального редактора
-				Autosave.fireEvent(inp, 'change');
-				Autosave.set(layer, inp.name, undefined);//В сессии установится null 
-				Autosave.bracket(inp, false);//Скрываем пипку сбороса сохранённого
-			});
-		}
-	});
-	//Функция сохраняет все значение, а не только того элемента на ком она сработала
-
-	Autosave.loadAll(layer);//Востанавливаем то что есть в autosave, При установки нового занчения срабатывает change
-	//change может программно вызываться у множества элементов. это приводит к тормозам.. нужно объединять
-	inps.change(function () {//Всё на change.. при авто изменении нужно вызывать событие change
-		//Autosave.saveAll(layer);
-		var inp = $(this);
-		var name = inp.attr('name');//getInps проверяет чтобы у всех были name
-		//this.removeAttribute('notautosaved');//должно быть отдельное событие которое при малейшем измееннии поля ввода будет удалять это свойство //Если свойства этого нет, то сохранять ничего не нужно
-
-		var val = Autosave.getVal(inp);
-		//var nowval=Autosave.get(layer,name);
-		//if(!nowval)nowval='';
-		//if(val===nowval)return;
-		Autosave.bracket(inp, true);
-		Autosave.set(layer, name, val);
-	});//Подписались на события inputов onchange
-}
 
 
-
-
-/*
-* При скрытии слоя сохраняем изменения в его полях 
-* 
-*/
-/* infra.listen(infra,'Layer.onhide',function(){//rte.layer.js сохраняется autosave в onhide - подставляетяс texarea после редактора
-	var layer=this;
-	if(!layer.autosavename)return;
-	if(layer.autosaveonhide===false)return;
-	Autosave.saveAll(layer);
-});*/
-
-window.Autosave = Controller.autosave = Autosave;//Это нужно из за метода clear который может вызываться кем угодно. и localSave
-
-//infra.seq.set(infra.template.scope,['autosave','get'],Autosave.get);
-//infra.seq.set(infra.template.scope,['infra','session','get'],Session.get);
+window.Autosave = Autosave;//Это нужно из за метода clear который может вызываться кем угодно. и localSave
 
 export {Autosave}
--- infra.js
+++ infra.js
@@ -1,20 +1,22 @@
 import { Event } from '/vendor/infrajs/event/Event.js'
+import { Controller } from '/vendor/infrajs/controller/src/Controller.js'
+import { External } from '/vendor/infrajs/controller/src/External.js'
 
-Event.handler('Layer.onshow', async (layer) => {
-	//autosave
-	Controller.autosaveHand(layer);
-}, 'autosave:dom');
+// Event.handler('Layer.onshow', async (layer) => {
+// 	if (layer.autosavenametpl) layer.autosavename = Template.parse([layer.autosavenametpl], layer);
+// 	if (!layer.autosavename) return;
+// 	Autosave.hand(layer.autosavename, layer.div);
+// }, 'autosave:dom');
 
-Event.one('Controller.oninit',async () => {
-	Controller.externalAdd('autosave', function(now, ext, layer, external) {
-		if (external.inheritance) {
-			now=ext;//Если есть метка о наследовании, то просто сохраняем указатель
-		} else {//Иначе копируем и при изменении одного другой изменяться не будет 
-			if (!now) now={};
-			for (var v in ext) {//Объект autosave не наследуется а копируется
-				now[v] = ext[v];
-			}
+
+External.add('autosave', function(now, ext, layer, external) {
+	if (external.inheritance) {
+		now=ext;//Если есть метка о наследовании, то просто сохраняем указатель
+	} else {//Иначе копируем и при изменении одного другой изменяться не будет 
+		if (!now) now={};
+		for (var v in ext) {//Объект autosave не наследуется а копируется
+			now[v] = ext[v];
 		}
-		return now;
-	});
+	}
+	return now;
 });
